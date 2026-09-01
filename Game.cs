4cd98407d2b7cde2acb3afb26b17873a737933b4
KalamitousN <nkcrit@gmail.com>
2026-09-01 14:00:03 +0200

    Cube
    Just a little test, working on Camera now...

--- Game.cs
+++ Game.cs
@@ -2,6 +2,7 @@ using OpenTK;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
 
 namespace PortalPrototype;
 
@@ -30,13 +31,15 @@ public class Game : GameWindow
         Console.WriteLine(
             $"GPU: {GL.GetString(StringName.Renderer)}");
 
+        GL.Enable(EnableCap.DepthTest);
+        
         GL.ClearColor(
             0.1f,
             0.1f,
             0.2f,
             1.0f);
 
-        CreateQuad();
+        CreateObject();
         CreateShaderProgram();
     }
 
@@ -50,20 +53,75 @@ public class Game : GameWindow
         GL.DeleteProgram(_shaderProgram);
     }
 
-    protected override void OnRenderFrame(FrameEventArgs args)
+   protected override void OnRenderFrame(
+        FrameEventArgs args)
     {
         base.OnRenderFrame(args);
 
-        GL.Clear(ClearBufferMask.ColorBufferBit);
+        GL.Clear(
+            ClearBufferMask.ColorBufferBit |
+            ClearBufferMask.DepthBufferBit);
 
         GL.UseProgram(_shaderProgram);
-        GL.BindVertexArray(_vertexArrayObject);
 
-        //GL.PointSize(16.0f);
+        Matrix4 model =
+            Matrix4.CreateRotationX(
+                MathHelper.DegreesToRadians(-20.0f))
+            *
+            Matrix4.CreateRotationY(
+                MathHelper.DegreesToRadians(35.0f));
+
+        Matrix4 view = Matrix4.LookAt(
+            new Vector3(0.0f, 0.0f, 3.0f),
+            Vector3.Zero,
+            Vector3.UnitY);
+
+        float aspectRatio =
+            Size.X / (float)Size.Y;
+
+        Matrix4 projection =
+            Matrix4.CreatePerspectiveFieldOfView(
+                MathHelper.DegreesToRadians(60.0f),
+                aspectRatio,
+                0.1f,
+                100.0f);
+
+        int modelLocation =
+            GL.GetUniformLocation(
+                _shaderProgram,
+                "model");
+
+        int viewLocation =
+            GL.GetUniformLocation(
+                _shaderProgram,
+                "view");
+
+        int projectionLocation =
+            GL.GetUniformLocation(
+                _shaderProgram,
+                "projection");
+
+        GL.UniformMatrix4(
+            modelLocation,
+            true,
+            ref model);
+
+        GL.UniformMatrix4(
+            viewLocation,
+            true,
+            ref view);
+
+        GL.UniformMatrix4(
+            projectionLocation,
+            true,
+            ref projection);
+
+        GL.BindVertexArray(
+            _vertexArrayObject);
 
         GL.DrawElements(
             PrimitiveType.Triangles,
-            6,
+            36,
             DrawElementsType.UnsignedInt,
             0);
 
@@ -81,29 +139,56 @@ public class Game : GameWindow
             e.Height);
     }
 
-    private void CreateQuad()
+    private void CreateObject()
     {
         float[] vertices =
         {
             // X Y Z R G B
-            -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,
-            0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,
-            0.5f,  0.5f, 0.0f,      0.0f, 0.0f, 1.0f,
-            -0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 0.0f
+
+            // Back
+            -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,     1.0f, 0.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,     1.0f, 1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,
+
+            // Front
+            -0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,     1.0f, 0.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,     1.0f, 1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 1.0f
         };
 
         uint[] indices =
         {
-            // 2 tris -> quad, would be cool if I could import .obj/.fbx files eventually...
+            // Back
             0, 1, 2,
-            2, 3, 0
+            2, 3, 0,
+
+            // Front
+            4, 5, 6,
+            6, 7, 4,
+
+            // Left
+            0, 4, 7,
+            7, 3, 0,
+
+            // Right
+            1, 5, 6,
+            6, 2, 1,
+
+            // Bottom
+            0, 1, 5,
+            5, 4, 0,
+
+            // Top
+            3, 2, 6,
+            6, 7, 3
         };
 
         _vertexArrayObject = GL.GenVertexArray();
         GL.BindVertexArray(_vertexArrayObject);
 
         _vertexBufferObject = GL.GenBuffer();
-
         GL.BindBuffer(
             BufferTarget.ArrayBuffer,
             _vertexBufferObject);
@@ -115,7 +200,6 @@ public class Game : GameWindow
             BufferUsageHint.StaticDraw);
 
         _elementBufferObject = GL.GenBuffer();
-
         GL.BindBuffer(
             BufferTarget.ElementArrayBuffer,
             _elementBufferObject);
